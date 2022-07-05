f619803d1fdb77522e00c5c524a41d376eb87047
Alejandro Montes <ango19941994@gmail.com>
2022-07-05 11:57:32 +0200

    Add authservice and login method to usercontroller

---
+++ dist/services/authorization.js
@@ -0,0 +1,16 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
+export const encrypt = async (source, salt = 10) => {
+    return await bcrypt.hash(source, salt);
+};
+export const compare = async (value, hash) => {
+    return await bcrypt.compare(value, hash);
+};
+export const createToken = (tokenPayLoad) => {
+    return jwt.sign(tokenPayLoad, process.env.SECRET);
+};
+export const verifyToken = (token) => {
+    return jwt.verify(token, process.env.SECRET);
+};
