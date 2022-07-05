4b532fbd5133fbe235e1411717323bc5d37fc6d9
Alejandro Montes <ango19941994@gmail.com>
2022-07-05 10:44:50 +0200

    Add user controller

---
+++ dist/routers/user.router.js
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import { UserController } from '../controllers/user.controller.js';
+export const userController = new UserController();
+export const userRouter = Router();
+userRouter.get('/:id', userController.getController);
+userRouter.post('/', userController.postController);
+userRouter.delete('/:id', userController.deleteController);
+userRouter.patch('/:id', userController.patchController);
--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import morgan from 'morgan';
 import cors from 'cors';
 import { artistRouter } from './routers/artist.router.js';
 import { comicRouter } from './routers/comic.router.js';
+import { userRouter } from './routers/user.router.js';
 
 export const app = express();
 app.use(morgan('dev'));
@@ -11,3 +12,4 @@ app.use(cors());
 
 app.use('/artist', artistRouter);
 app.use('/comic', comicRouter);
+app.use('/user', userRouter);
